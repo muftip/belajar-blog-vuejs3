bc597d04b3239d6f8c08132aee74ca457ad3096f
muftip <muftipurwa4@gmail.com>
2024-12-08 20:50:05 +0700

    Initial commit with project files

---
+++ src/components/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="post-list">
+      <div class="post-preview" v-for="(item, index) in posts" :key="index">
+          <PostSingle :data_item="item" />
+          <hr> 
+      </div>
+      <!-- Divider-->
+      <hr class="my-4" />
+        <!-- Pager-->
+      <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
+        
+  </div>
+</template>
+<script>
+
+
+import PostSingle from '../posts/PostSingle.vue';
+import { onMounted, onUnmounted, onUpdated } from 'vue';
+export default {
+  props:['posts'],
+  components:{
+      PostSingle
+  },
+  setup(props) {
+      // console.log(props.posts);
+      onMounted(()=>console.log("component onMounted"))
+      onUnmounted(()=>console.log("component onUnmounted"))
+      onUpdated(()=>console.log("component onUpdate"))
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <!-- Passing posts array to the PostList component -->
+    <PostList :post="posts"/>
+  </div>
+</template>
+
+<script>
+import PostList from '../components/PostList';  // Importing the PostList component
+import { ref } from 'vue';
+
+export default {
+  name: 'Home',
+  components: {
+    PostList
+  },
+  setup() {
+    const posts = ref([
+      {
+        id: 1, title: 'Artikel pertama di februari',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      },
+      {
+        id: 2, title: 'Artikel kedua di februari',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }
+    ]);
+
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
