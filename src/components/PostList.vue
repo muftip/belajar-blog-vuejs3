e475448d091b7783ff622e2b9cac86fbd0ad2635
muftip <muftipurwa4@gmail.com>
2024-12-08 21:05:03 +0700

    Deskripsi perubahan yang Anda buat

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,31 +1,18 @@
 <template>
-  <div class="post-list">
-      <div class="post-preview" v-for="(item, index) in posts" :key="index">
-          <PostSingle :data_item="item" />
-          <hr> 
-      </div>
-      <!-- Divider-->
-      <hr class="my-4" />
-        <!-- Pager-->
-      <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
-        
+  <div class="post-list"> 
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{ post.title }}</h3>
   </div>
+</div>
 </template>
+
+
 <script>
-
-
-import PostSingle from '../posts/PostSingle.vue';
-import { onMounted, onUnmounted, onUpdated } from 'vue';
 export default {
-  props:['posts'],
-  components:{
-      PostSingle
-  },
-  setup(props) {
-      // console.log(props.posts);
-      onMounted(()=>console.log("component onMounted"))
-      onUnmounted(()=>console.log("component onUnmounted"))
-      onUpdated(()=>console.log("component onUpdate"))
-  }
-}
+ props: ['posts']
+};
 </script>
+
+<style>
+
+</style>
